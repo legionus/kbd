e032b40f3a5a14cd042f8ec4a12ea0fc721b9a82
Benno Schulenberg <bensberg@justemail.net>
2014-05-26 22:40:29 +0200

    Add the handling of plurals to the extraction of translatable strings.
    Signed-off-by: Benno Schulenberg <bensberg@justemail.net>

--- src/nls.h
+++ src/nls.h
@@ -17,6 +17,7 @@
 # else
 #  define N_(String) (String)
 # endif
+# define P_(singular, plural, number) ngettext(singular, plural, number)
 #else
 # undef bindtextdomain
 # define bindtextdomain(Domain, Directory) /* empty */
@@ -24,6 +25,5 @@
 # define textdomain(Domain) /* empty */
 # define _(Text) (Text)
 # define N_(Text) (Text)
+# define P_(singular, plural, number) (number == 1 ? singular : plural)
 #endif
-
-
