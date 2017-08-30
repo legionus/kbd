1b93ed6b5d2673c20ba6143a178e7c070ab2552e
Alexey Gladkov <gladkov.alexey@gmail.com>
2017-08-30 14:06:50 +0200

    Rename libfindfile -> src/libkbdfile
    Signed-off-by: Alexey Gladkov <gladkov.alexey@gmail.com>

---
+++ src/readpsfheader.c
@@ -0,0 +1,49 @@
+#include "config.h"
+
+#include <stdio.h>
+#include <stdlib.h> /* exit */
+#include "nls.h"
+#include "getfd.h"
+#include "version.h"
+#include "kdmapop.h"
+#include "kdfontop.h"
+#include "kbd_error.h"
+#include "xmalloc.h"
+
+int verbose = 0;
+int debug   = 0;
+
+static void __attribute__((noreturn))
+usage(void)
+{
+	fprintf(stderr, "usage: readpsfheader font.psf\n");
+	exit(1);
+}
+
+int main(int argc, char **argv)
+{
+	int psftype, fontlen, charsize, hastable, notable;
+	int width = 8, bytewidth, height;
+	char *inbuf, *fontbuf;
+	int inbuflth, fontbuflth;
+	struct unicode_list *uclistheads;
+
+	set_progname(argv[0]);
+
+	if (argc != 2)
+		usage();
+
+	FILE *f = fopen(argv[1], "rb");
+	if (!f) {
+		perror("fopen");
+		exit(1);
+	}
+
+	if (readpsffont(f, &inbuf, &inbuflth, &fontbuf, &fontbuflth, &width, &fontlen, 0, &uclistheads) == -1) {
+		fprintf(stderr, "%s: Bad magic number\n", argv[0]);
+		return 1;
+	}
+	close(f);
+
+	return 0;
+}
