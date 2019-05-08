184c028d774709901ece03aa64e829e9aaa45453
Alexey Gladkov <gladkov.alexey@gmail.com>
2019-05-08 11:04:55 +0200

    Add help and version options
    Signed-off-by: Alexey Gladkov <gladkov.alexey@gmail.com>

--- src/deallocvt.c
+++ src/deallocvt.c
@@ -13,12 +13,33 @@
 #include <sys/types.h>
 #include <sys/ioctl.h>
 #include <linux/vt.h>
+#include <getopt.h>
 
 #include "libcommon.h"
+
+static void __attribute__((noreturn))
+usage(int rc)
+{
+	fprintf(stderr, _("Usage: %s [option...] [N ...]\n"
+	                  "\n"
+	                  "Valid options are:\n"
+	                  "\n"
+	                  "  -h --help          display this help text\n"
+	                  "  -V --version       print version number\n"),
+		get_progname());
+	exit(rc);
+}
 
 int main(int argc, char *argv[])
 {
 	int fd, num, i;
+
+	const char *const short_opts = "hV";
+	const struct option long_opts[] = {
+		{ "help",    no_argument, NULL, 'h' },
+		{ "version", no_argument, NULL, 'V' },
+		{ NULL, 0, NULL, 0 }
+	};
 
 	if (argc < 1) /* unlikely */
 		return EXIT_FAILURE;
@@ -28,10 +49,19 @@ int main(int argc, char *argv[])
 	bindtextdomain(PACKAGE_NAME, LOCALEDIR);
 	textdomain(PACKAGE_NAME);
 
-	if (argc == 2 && !strcmp(argv[1], "-V"))
-		print_version_and_exit();
+	while ((i = getopt_long(argc, argv, short_opts, long_opts, NULL)) != -1) {
+		switch (i) {
+			case 'V':
+				print_version_and_exit();
+				break;
+			case 'h':
+				usage(EXIT_SUCCESS);
+			case '?':
+				usage(EXIT_FAILURE);
+		}
+	}
 
-	for (i = 1; i < argc; i++) {
+	for (i = optind; i < argc; i++) {
 		if (!isdigit(argv[i][0])) {
 			fprintf(stderr, _("%s: unknown option\n"), get_progname());
 			return EXIT_FAILURE;
