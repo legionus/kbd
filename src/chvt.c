af7aa344b1be7e941b6a602d16c0b4474e1b60a1
Alexey Gladkov <gladkov.alexey@gmail.com>
2019-10-15 09:55:04 +0200

    chvt: Add deadline to change virtual terminal
    Signed-off-by: Alexey Gladkov <gladkov.alexey@gmail.com>

--- src/chvt.c
+++ src/chvt.c
@@ -6,6 +6,7 @@
 #include <sys/types.h>
 #include <sys/ioctl.h>
 #include <linux/vt.h>
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -22,20 +23,29 @@ usage(int rc)
 	fprintf(stderr, _("Usage: %s [option...] N\n"
 	                  "\n"
 	                  "Options:\n"
+	                  "  -t, --timeout=NUM     set deadline to NUM seconds to change virtual terminal;\n"
 	                  "  -h, --help            print this usage message;\n"
 	                  "  -V, --version         print version number.\n"),
 		get_progname());
 	exit(rc);
 }
 
+static void __attribute__((noreturn))
+sigalrmhandler(int sig __attribute__((unused)))
+{
+	kbd_error(EXIT_FAILURE, 0, _("Failed to change virtual terminal"));
+}
+
 int main(int argc, char *argv[])
 {
 	int c, fd, num;
+	int secs = 0;
 
-	const char *const short_opts = "hV";
+	const char *const short_opts = "t:hV";
 	const struct option long_opts[] = {
 		{ "help",    no_argument, NULL, 'h' },
 		{ "version", no_argument, NULL, 'V' },
+		{ "timeout", required_argument, NULL, 't' },
 		{ NULL, 0, NULL, 0 }
 	};
 
@@ -44,6 +54,9 @@ int main(int argc, char *argv[])
 
 	while ((c = getopt_long(argc, argv, short_opts, long_opts, NULL)) != -1) {
 		switch (c) {
+			case 't':
+				secs = atoi(optarg);
+				break;
 			case 'V':
 				print_version_and_exit();
 				break;
@@ -65,6 +78,11 @@ int main(int argc, char *argv[])
 		kbd_error(EXIT_FAILURE, 0, _("Couldn't get a file descriptor referring to the console"));
 
 	num = atoi(argv[optind]);
+
+	if (secs > 0) {
+		signal(SIGALRM, sigalrmhandler);
+		alarm((unsigned int) secs);
+	}
 
 	if (ioctl(fd, VT_ACTIVATE, num)) {
 		kbd_error(EXIT_FAILURE, errno, "ioctl VT_ACTIVATE");
