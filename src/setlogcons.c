3ed9f2fa93052beb8818082a16efd7cc773263dd
Alexey Gladkov <gladkov.alexey@gmail.com>
2020-04-15 19:33:03 +0200

    setlogcons: Add more checks for argument
    Signed-off-by: Alexey Gladkov <gladkov.alexey@gmail.com>

--- src/setlogcons.c
+++ src/setlogcons.c
@@ -7,12 +7,17 @@
 /* Send kernel messages to the current console or to console N */
 #include "config.h"
 
+#include <linux/tiocl.h>
+#include <sys/ioctl.h>
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <stdlib.h>
-#include <sys/ioctl.h>
+#include <limits.h>
+#include <sysexits.h>
 
 #include "libcommon.h"
 
@@ -26,18 +31,37 @@ int main(int argc, char **argv)
 	set_progname(argv[0]);
 	setuplocale();
 
-	if (argc == 2)
+	cons = 0; /* current console */
+
+	if (argc == 2) {
+		if (!strcmp(argv[1], "-h") || !strcmp(argv[1], "--help")) {
+			fprintf(stderr, "Usage: %s [N]\n", get_progname());
+			fprintf(stderr, "Send kernel messages to the current console or to console N\n");
+			return EX_USAGE;
+		}
+
+		if (!strcmp(argv[1], "-V") || !strcmp(argv[1], "--version"))
+			print_version_and_exit();
+
 		cons = atoi(argv[1]);
-	else
-		cons = 0; /* current console */
+	}
+
+	if (cons < 0)
+		kbd_error(EX_DATAERR, 0, "Argument must be positive: %s",
+				(argc == 2) ? argv[1] : "0");
+
+	if (cons > CHAR_MAX)
+		kbd_error(EX_DATAERR, 0, "Argument is too big: %s",
+				(argc == 2) ? argv[1] : "0");
 
 	if ((fd = getfd(NULL)) < 0)
-		kbd_error(EXIT_FAILURE, 0, _("Couldn't get a file descriptor referring to the console"));
+		kbd_error(EX_OSERR, 0, _("Couldn't get a file descriptor referring to the console"));
 
-	arg.fn     = 11;   /* redirect kernel messages */
-	arg.subarg = cons; /* to specified console */
-	if (ioctl(fd, TIOCLINUX, &arg)) {
-		kbd_error(EXIT_FAILURE, errno, "TIOCLINUX");
-	}
-	return EXIT_SUCCESS;
+	arg.fn     = TIOCL_SETKMSGREDIRECT; /* redirect kernel messages */
+	arg.subarg = (char) cons;           /* to specified console */
+
+	if (ioctl(fd, TIOCLINUX, &arg))
+		kbd_error(EX_OSERR, errno, "TIOCLINUX");
+
+	return EX_OK;
 }
