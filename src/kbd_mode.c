legionus/kbd

commit c784dde1b5aee91373bac8f4e175f99988d61c90

Improve and unify error some messages
Signed-off-by: Alexey Gladkov <gladkov.alexey@gmail.com>

src/kbd_mode.c
@@ -57,20 +57,21 @@ fprint_mode(FILE *stream, int  mode)
 {
 	switch (mode) {
 		case K_RAW:
-			fprintf(stream, _("The keyboard is in raw (scancode) mode\n"));
+			fprintf(stream, _("The keyboard is in raw (scancode) mode"));
 			break;
 		case K_MEDIUMRAW:
-			fprintf(stream, _("The keyboard is in mediumraw (keycode) mode\n"));
+			fprintf(stream, _("The keyboard is in mediumraw (keycode) mode"));
 			break;
 		case K_XLATE:
-			fprintf(stream, _("The keyboard is in the default (ASCII) mode\n"));
+			fprintf(stream, _("The keyboard is in the default (ASCII) mode"));
 			break;
 		case K_UNICODE:
-			fprintf(stream, _("The keyboard is in Unicode (UTF-8) mode\n"));
+			fprintf(stream, _("The keyboard is in Unicode (UTF-8) mode"));
 			break;
 		default:
-			fprintf(stream, _("The keyboard is in some unknown mode\n"));
-        }
+			fprintf(stream, _("The keyboard is in some unknown mode"));
+	}
+	printf("\n");
 }
 
 int main(int argc, char *argv[])
@@ -152,22 +153,21 @@ int main(int argc, char *argv[])
 	}
 
 	if ((fd = getfd(console)) < 0)
-		kbd_error(EXIT_FAILURE, 0, _("Couldn't get a file descriptor referring to the console"));
+		kbd_error(EXIT_FAILURE, 0, _("Couldn't get a file descriptor referring to the console."));
 
 	if (n == 0) {
 		/* report mode */
-		if (ioctl(fd, KDGKBMODE, &mode)) {
-			kbd_error(EXIT_FAILURE, errno, "ioctl KDGKBMODE");
-		}
+		if (ioctl(fd, KDGKBMODE, &mode))
+			kbd_error(EXIT_FAILURE, errno, _("Unable to read keyboard mode"));
+
 		fprint_mode(stdout, mode);
 		return EXIT_SUCCESS;
 	}
 
 	if (force == 0) {
 		/* only perform safe mode switches */
-		if (ioctl(fd, KDGKBMODE, &orig_mode)) {
-			kbd_error(EXIT_FAILURE, errno, "ioctl KDGKBMODE");
-		}
+		if (ioctl(fd, KDGKBMODE, &orig_mode))
+			kbd_error(EXIT_FAILURE, errno, _("Unable to read keyboard mode"));
 
 		if (mode == orig_mode) {
 			/* skip mode change */

src/chvt.c
@@ -95,12 +95,12 @@ int main(int argc, char *argv[])
 	}
 
 	if (argc == optind) {
-		fprintf(stderr, _("Argument required\n"));
+		kbd_warning(0, _("Not enough arguments."));
 		usage(EX_USAGE, opthelp);
 	}
 
 	if ((fd = getfd(NULL)) < 0)
-		kbd_error(EXIT_FAILURE, 0, _("Couldn't get a file descriptor referring to the console"));
+		kbd_error(EXIT_FAILURE, 0, _("Couldn't get a file descriptor referring to the console."));
 
 	num = atoi(argv[optind]);
 
@@ -109,14 +109,14 @@ int main(int argc, char *argv[])
 	sigemptyset(&sa.sa_mask);
 
 	if (sigaction(SIGALRM, &sa, NULL) < 0)
-		kbd_error(EXIT_FAILURE, errno, "sigaction");
+		kbd_error(EXIT_FAILURE, errno, _("Unable to set signal handler"));
 
 	sev.sigev_notify = SIGEV_SIGNAL;
 	sev.sigev_signo = SIGALRM;
 	sev.sigev_value.sival_ptr = &timerid;
 
 	if (timer_create(CLOCK_REALTIME, &sev, &timerid) < 0)
-		kbd_error(EXIT_FAILURE, errno, "timer_create");
+		kbd_error(EXIT_FAILURE, errno, _("Unable to create timer"));
 
 	its.it_value.tv_sec     = 1;
 	its.it_value.tv_nsec    = 0;
@@ -124,7 +124,7 @@ int main(int argc, char *argv[])
 	its.it_interval.tv_nsec = its.it_value.tv_nsec;
 
 	if (timer_settime(timerid, 0, &its, NULL) < 0)
-		kbd_error(EXIT_FAILURE, errno, "timer_settimer");
+		kbd_error(EXIT_FAILURE, errno, _("Unable to set timer"));
 
 	do {
 		errno = 0;

src/clrunimap.c
@@ -26,7 +26,7 @@ int main(int argc, char *argv[])
 		console = argv[2];
 
 	if ((fd = getfd(console)) < 0)
-		kbd_error(EXIT_FAILURE, 0, _("Couldn't get a file descriptor referring to the console"));
+		kbd_error(EXIT_FAILURE, 0, _("Couldn't get a file descriptor referring to the console."));
 
 	int ret;
 	struct kfont_context *ctx;

src/deallocvt.c
@@ -85,13 +85,13 @@ int main(int argc, char *argv[])
 
 	for (i = optind; i < argc; i++) {
 		if (!isdigit(argv[i][0])) {
-			fprintf(stderr, _("%s: unknown option\n"), get_progname());
+			kbd_warning(0, _("Unrecognized argument: %s"), argv[i]);
 			return EX_USAGE;
 		}
 	}
 
 	if ((fd = getfd(NULL)) < 0)
-		kbd_error(EXIT_FAILURE, 0, _("Couldn't get a file descriptor referring to the console"));
+		kbd_error(EXIT_FAILURE, 0, _("Couldn't get a file descriptor referring to the console."));
 
 	if (argc == 1) {
 		/* deallocate all unused consoles */

src/dumpkeys.c
@@ -187,11 +187,11 @@ int main(int argc, char *argv[])
 		usage(EX_USAGE, opthelp);
 
 	if ((fd = getfd(console)) < 0)
-		kbd_error(EXIT_FAILURE, 0, _("Couldn't get a file descriptor referring to the console"));
+		kbd_error(EXIT_FAILURE, 0, _("Couldn't get a file descriptor referring to the console."));
 
 	/* check whether the keyboard is in Unicode mode */
 	if (ioctl(fd, KDGKBMODE, &kbd_mode))
-		kbd_error(EXIT_FAILURE, errno, _("error reading keyboard mode"));
+		kbd_error(EXIT_FAILURE, errno, _("Unable to read keyboard mode"));
 
 	if (kbd_mode == K_UNICODE) {
 		lk_set_parser_flags(ctx, LK_FLAG_PREFER_UNICODE);

src/fgconsole.c
@@ -93,7 +93,7 @@ int main(int argc, char **argv)
 	}
 
 	if ((fd = getfd(console)) < 0)
-		kbd_error(EXIT_FAILURE, 0, _("Couldn't get a file descriptor referring to the console"));
+		kbd_error(EXIT_FAILURE, 0, _("Couldn't get a file descriptor referring to the console."));
 
 	if (show_vt) {
 		if ((ioctl(fd, VT_OPENQRY, &vtno) < 0) || vtno == -1) {

src/getkeycodes.c
@@ -84,7 +84,7 @@ int main(int argc, char **argv)
 	}
 
 	if ((fd = getfd(NULL)) < 0)
-		kbd_error(EXIT_FAILURE, 0, _("Couldn't get a file descriptor referring to the console"));
+		kbd_error(EXIT_FAILURE, 0, _("Couldn't get a file descriptor referring to the console."));
 
 	/* Old kernels don't support changing scancodes below SC_LIM. */
 	a.scancode = 0;

src/getunimap.c
@@ -110,7 +110,7 @@ int main(int argc, char **argv)
 		usage(EX_USAGE, opthelp);
 
 	if ((fd = getfd(console)) < 0)
-		kbd_error(EXIT_FAILURE, 0, _("Couldn't get a file descriptor referring to the console"));
+		kbd_error(EXIT_FAILURE, 0, _("Couldn't get a file descriptor referring to the console."));
 
 	int ret;
 	struct kfont_context *kfont;

commit 2dc95f8b51ccf851c94c5414564337eb832f8fb5

Fix kbd_mode output
The newline is printed to stdout, instead of stream.

Fixes: c784dde ("Improve and unify error some messages")
Signed-off-by: Alexey Gladkov <gladkov.alexey@gmail.com>

src/kbd_mode.c
@@ -71,7 +71,7 @@ fprint_mode(FILE *stream, int  mode)
 		default:
 			fprintf(stream, _("The keyboard is in some unknown mode"));
 	}
-	printf("\n");
+	fprintf(stream, "\n");
 }
 
 int main(int argc, char *argv[])