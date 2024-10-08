5faa6bb4e81499acb7c254257973c14a813d52bf
Alexey Gladkov <legion@kernel.org>
2024-10-08 16:07:08 +0200

    Convert compiler attributes in public headers
    Signed-off-by: Alexey Gladkov <legion@kernel.org>

--- src/include/kbd/keymap/logging.h
+++ src/include/kbd/keymap/logging.h
@@ -12,14 +12,9 @@
 
 #include <kbd/keymap/context.h>
 
-#ifndef __GNUC__
-#undef  __attribute__
-#define __attribute__(x) /*NOTHING*/
-#endif
-
 typedef void (*lk_logger_t)(void *, int, const char *, int, const char *, const char *, va_list)
-	__attribute__((nonnull(1)))
-	__attribute__((format(printf, 6, 0)));
+	KBD_ATTR_PRINTF(6, 0)
+	KBD_ATTR_NONNULL(1);
 
 /**
  * Logging function which uses @ref lk_ctx::log_fn "log_fn" and
@@ -29,9 +24,9 @@ typedef void (*lk_logger_t)(void *, int, const char *, int, const char *, const 
  */
 void
 lk_log(struct lk_ctx *ctx, int priority,
-       const char *file, int line, const char *fn,
-       const char *fmt, ...)
-       __attribute__((format(printf, 6, 7)))
-       __attribute__((nonnull(1)));
+		const char *file, int line, const char *fn,
+		const char *fmt, ...)
+	KBD_ATTR_PRINTF(6, 7)
+	KBD_ATTR_NONNULL(1);
 
 #endif /* _KBD_LIBKEYMAP_LOGGING_H_ */
